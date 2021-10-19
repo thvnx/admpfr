819ea18a6d58c8b78de886156b4c390af0319cbb
Laurent Thévenoux <laurent@thevenoux.net>
2021-10-19 21:37:59 +0200

    Add a bit of MPFR
    Build with (at least on macOS):
    `LIBRARY_PATH=/path/to/mpfr/lib gprbuild -p`.
    
    Make sure to have gnat last in your path to use your C compiler first.

--- src/my_func.c
+++ src/my_func.c
@@ -1,6 +1,15 @@
+#include <mpfr.h>
+
 #include "my_func.h"
 
 int my_func (int a)
 {
-  return a * 2;
+  mpfr_t t;
+  long r;
+  mpfr_init2 (t, 200);
+  mpfr_set_d (t, (double)a, MPFR_RNDD);
+  mpfr_mul_ui (t, t, 3, MPFR_RNDU);
+  r = mpfr_get_si (t, MPFR_RNDN);
+  mpfr_clear (t);
+  return r;
 }
