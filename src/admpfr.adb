796c1cd5d7abd4d39a883d9afbeb05649db69c28
Laurent Thévenoux <laurent@thevenoux.net>
2021-10-31 21:35:52 +0100

    Set AdMPFR license to GPLv3
    https://www.gnu.org/licenses/gpl-howto.en.html

--- src/admpfr.adb
+++ src/admpfr.adb
@@ -1,4 +1,19 @@
-with Ada.Text_IO;           use Ada.Text_IO;
+--  This file is part of AdMPFR.
+--
+--  AdMPFR is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  AdMPFR is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
+With Ada.Text_IO;           use Ada.Text_IO;
 with Ada.Strings;           use Ada.Strings;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Interfaces.C;          use Interfaces.C;
