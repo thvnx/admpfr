844413afbecaee1efb8eb9fd7b85e6741ffc9ef8
Laurent Thévenoux <laurent@thevenoux.net>
2021-10-31 22:29:08 +0100

    fixup! Remove initial C binding test

--- src/admpfr_stubs.c
+++ src/admpfr_stubs.c
@@ -17,17 +17,6 @@
 
 #include "admpfr_stubs.h"
 
-int my_func (int a)
-{
-  mpfr_t t;
-  long r;
-  mpfr_init2 (t, 200);
-  mpfr_set_d (t, (double)a, MPFR_RNDD);
-  mpfr_mul_ui (t, t, 3, MPFR_RNDU);
-  r = mpfr_get_si (t, MPFR_RNDN);
-  mpfr_clear (t);
-  return r;
-}
 
 int rnd_t_pos_to_int (int r)
 {
--- src/admpfr_stubs.h
+++ src/admpfr_stubs.h
@@ -16,7 +16,6 @@
 #ifndef ADMPFR_STUBS_H
 #define ADMPFR_STUBS_H
 
-int my_func (int a);
 int rnd_t_pos_to_int (int r);
 
 #endif /* ADMPFR_STUBS_H */
