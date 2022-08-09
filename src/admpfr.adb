76acfff0f0c1a9a672a7ea102a9ae2b3abd9d129
Laurent Thévenoux <laurent@thevenoux.net>
2022-08-09 22:29:54 +0200

    Add Mul bindings

--- src/admpfr.adb
+++ src/admpfr.adb
@@ -692,6 +692,56 @@ package body Admpfr is
                                       Rounding'Pos (Rnd)));
    end Sub;
 
+   ---------
+   -- Mul --
+   ---------
+
+   procedure Mul
+     (Rop      : in out Mpfloat;
+      Op1, Op2 : Mpfloat;
+      Rnd      : Rounding := RNDN) is
+   begin
+      Rop.Ternary :=
+        To_Ternary_Value (mpfr_mul (Rop.Value'Access,
+                                    Op1.Value'Access,
+                                    Op2.Value'Access,
+                                    Rounding'Pos (Rnd)));
+   end Mul;
+
+   ---------
+   -- Mul --
+   ---------
+
+   procedure Mul
+     (Rop : in out Mpfloat;
+      Op1 : Mpfloat;
+      Op2 : Long_Integer;
+      Rnd : Rounding := RNDN) is
+   begin
+      Rop.Ternary :=
+        To_Ternary_Value (mpfr_mul_si (Rop.Value'Access,
+                                       Op1.Value'Access,
+                                       long (Op2),
+                                       Rounding'Pos (Rnd)));
+   end Mul;
+
+   ---------
+   -- Mul --
+   ---------
+
+   procedure Mul
+     (Rop : in out Mpfloat;
+      Op1 : Mpfloat;
+      Op2 : Long_Float;
+      Rnd : Rounding := RNDN) is
+   begin
+      Rop.Ternary :=
+        To_Ternary_Value (mpfr_mul_d (Rop.Value'Access,
+                                      Op1.Value'Access,
+                                      double (Op2),
+                                      Rounding'Pos (Rnd)));
+   end Mul;
+
    --------------
    -- Get_Prec --
    --------------
---
+++ testsuite/tests/mul/test.adb
@@ -0,0 +1,18 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Admpfr;      use Admpfr;
+
+procedure Test is
+   M, N, O : Mpfloat;
+begin
+   N.Set (Float (1.5));
+   O.Set (Float (4.0));
+
+   M.Mul (N, O);
+   Put_Line (M'Image);
+
+   M.Mul (N, 5);
+   Put_Line (M'Image);
+
+   M.Mul (N, 6.0);
+   Put_Line (M'Image);
+end Test;
