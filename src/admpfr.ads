7e4af45f265b8f702102f0aa32a903715d1dd7df
Laurent Thévenoux <laurent@thevenoux.net>
2022-09-11 21:50:53 +0200

    Fix license header

--- src/admpfr.ads
+++ src/admpfr.ads
@@ -1,17 +1,17 @@
---  This file is part of AdMPFR.
+--  This file is part of Admpfr.
 --
---  AdMPFR is free software: you can redistribute it and/or modify
+--  Admpfr is free software: you can redistribute it and/or modify
 --  it under the terms of the GNU General Public License as published by
 --  the Free Software Foundation, either version 3 of the License, or
 --  (at your option) any later version.
 --
---  AdMPFR is distributed in the hope that it will be useful,
+--  Admpfr is distributed in the hope that it will be useful,
 --  but WITHOUT ANY WARRANTY; without even the implied warranty of
 --  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 --  GNU General Public License for more details.
 --
 --  You should have received a copy of the GNU General Public License
---  along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+--  along with Admpfr.  If not, see <https://www.gnu.org/licenses/>.
 
 with Ada.Finalization; use Ada.Finalization;
 with Ada.Text_IO;      use Ada.Text_IO;
